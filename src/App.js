f635ad63263f23602baea75506d8ebf893f05934
md-merajuddin_CEQEMU <mdmerajuddin@cloudeq.com>
2024-09-17 15:17:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
-function App() {
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
+
+  const login = useCallback(uid => {
+    setIsLoggedIn(true);
+    setUserId(uid);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setUserId(null);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>
+            {isLoggedIn && (
+              <>
+                <Route path="/" element={<Users />} />
+                <Route path="/:userId/places" element={<UserPlaces />} />
+                <Route path="/places/new" element={<NewPlace />} />
+                <Route path="/places/:placeId" element={<UpdatePlace />} />
+                <Route path="*" element={<Navigate to="/" />} />
+              </>
+            )}
+            {!isLoggedIn && (
+              <>
+                <Route path="/" element={<Users />} />
+                <Route path="/:userId/places" element={<UserPlaces />} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="*" element={<Navigate to="/auth" />} />
+              </>
+            )}
+          </Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
-}
+};
 
 export default App;
